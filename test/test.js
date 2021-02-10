3645379428b7ac6b81375d57fe1017611a5ee5a2
Ken MacKeen <wellnessgardener@gmail.com>
2021-02-10 13:07:15 -0500

    first commit

--- test/test.js
+++ test/test.js
@@ -23,21 +23,21 @@ contract('Decentragram', ([deployer, author, tipper]) => {
     it('has a name', async () => {
       const name = await decentragram.name()
       assert.equal(name, 'Decentragram')
+ 
+
     })
   })
-
   describe('images', async () => {
     let result, imageCount
-    const hash = 'QmV8cfu6n4NT5xRr2AHdKxFMTZEJrA44qgrBCr739BN9Wb'
+    const hash = 'abc123'
 
     before(async () => {
-      result = await decentragram.uploadImage(hash, 'Image description', { from: author })
-      imageCount = await decentragram.imageCount()
-    })
+    result = await decentragram.uploadImage(hash, 'Image description', { from: author})
+    imageCount = await decentragram.imageCount()
+  })
 
-    //check event
     it('creates images', async () => {
-      // SUCESS
+      // SUCCESS
       assert.equal(imageCount, 1)
       const event = result.logs[0].args
       assert.equal(event.id.toNumber(), imageCount.toNumber(), 'id is correct')
@@ -46,15 +46,14 @@ contract('Decentragram', ([deployer, author, tipper]) => {
       assert.equal(event.tipAmount, '0', 'tip amount is correct')
       assert.equal(event.author, author, 'author is correct')
 
-
       // FAILURE: Image must have hash
       await decentragram.uploadImage('', 'Image description', { from: author }).should.be.rejected;
 
       // FAILURE: Image must have description
-      await decentragram.uploadImage('Image hash', '', { from: author }).should.be.rejected;
+      await decentragram.uploadImage('Image hash', '', { from: author }).should.be.rejected;  
     })
 
-    //check from Struct
+    //  check from Struct
     it('lists images', async () => {
       const image = await decentragram.images(imageCount)
       assert.equal(image.id.toNumber(), imageCount.toNumber(), 'id is correct')
@@ -63,8 +62,7 @@ contract('Decentragram', ([deployer, author, tipper]) => {
       assert.equal(image.tipAmount, '0', 'tip amount is correct')
       assert.equal(image.author, author, 'author is correct')
     })
-
-    it('allows users to tip images', async () => {
+it('allows users to tip images', async () => {
       // Track the author balance before purchase
       let oldAuthorBalance
       oldAuthorBalance = await web3.eth.getBalance(author)
