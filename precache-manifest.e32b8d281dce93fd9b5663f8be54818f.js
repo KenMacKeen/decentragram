b508663a9fe51b12a379bfd866273fa26596f3e9
Ken MacKeen <wellnessgardener@gmail.com>
2021-02-12 10:46:29 -0500

    Updates

---
+++ precache-manifest.e32b8d281dce93fd9b5663f8be54818f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d8c9ecf41e8e17de51",
+    "url": "/decentragram/static/css/main.a0722297.chunk.css"
+  },
+  {
+    "revision": "a7d8c9ecf41e8e17de51",
+    "url": "/decentragram/static/js/main.a7d8c9ec.chunk.js"
+  },
+  {
+    "revision": "e8647a5a5aef416cbd9f",
+    "url": "/decentragram/static/css/1.3b894062.chunk.css"
+  },
+  {
+    "revision": "e8647a5a5aef416cbd9f",
+    "url": "/decentragram/static/js/1.e8647a5a.chunk.js"
+  },
+  {
+    "revision": "a4a45a276db0bfa179c8",
+    "url": "/decentragram/static/js/runtime~main.a4a45a27.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/decentragram/static/media/photo.d6cbd8e6.png"
+  },
+  {
+    "revision": "27f8bbe9bff8f9f79a90b88e684cb359",
+    "url": "/decentragram/index.html"
+  }
+];
