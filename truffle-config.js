c8181802c7518f0e80aca2947eca7208eed56578
Ken MacKeen <wellnessgardener@gmail.com>
2021-02-12 13:15:41 -0500

    add kovan

--- truffle-config.js
+++ truffle-config.js
@@ -3,10 +3,10 @@ require('babel-polyfill');
 
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+    kovan: {
+      host: "localhost",
+      port: 8545,
+      network_id: "42" // Kovan network id
     },
   },
   contracts_directory: './src/contracts/',
